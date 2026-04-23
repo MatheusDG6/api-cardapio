62bf0b92fdd97ed9bf99e56c3a5a0f520d29a8b9
Matheus <matheusdg.2006@gmail.com>
2026-04-23 16:00:52 -0300

    iniciando projeto

---
+++ src/main/java/com/ecardapio/democardapio/DemocardapioApplication.java
@@ -0,0 +1,13 @@
+package com.ecardapio.democardapio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocardapioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocardapioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecardapio/democardapio/DemocardapioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecardapio.democardapio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemocardapioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
